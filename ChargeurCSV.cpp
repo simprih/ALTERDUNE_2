b5f6aaf83fbf2a078df456f06800f1dd209ff597
simprih <simon.prihnenko@edu.devinci.fr>
2026-04-10 14:35:58 +0200

    5_fichier main et corrections pour suivi, le code compile

--- ChargeurCSV.cpp
+++ ChargeurCSV.cpp
@@ -11,16 +11,17 @@ vector<Item> ChargeurCSV::chargerItems(const string& fichier) {
 
     getline(file, ligne); // on saute l'en-tête
 
-    while (getline(file, ligne)) {
-        stringstream ss(ligne);
+    while (getline(file, ligne)) // tant qu'on lit une ligne
+    { 
+        stringstream ss(ligne); // tranformer la ligne en flux pour la lire morceaux par morceaux
         string nom, type, valeurStr, quantiteStr;
 
-        getline(ss, nom, ';');
-        getline(ss, type, ';');
-        getline(ss, valeurStr, ';');
-        getline(ss, quantiteStr, ';');
+        getline(ss, nom, ';'); // attribut ce qui est lu de ss à ; à nom 
+        getline(ss, type, ';'); // attribut ce qui est lu jusqu'au prochain ;
+        getline(ss, valeurStr, ';'); //...
+        getline(ss, quantiteStr, ';'); //...
 
-        items.push_back(Item(nom, type, stoi(valeurStr), stoi(quantiteStr)));
+        items.push_back(Item(nom, type, stoi(valeurStr), stoi(quantiteStr))); // ajoute l'objet dans le vecteur items ; stoi = string to int 
     }
 
     file.close();
